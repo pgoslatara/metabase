840dde5f571abf12f6080bf043f9aa65fa4c8a93
Romeo Van Snick <romeo@romeovansnick.be>
2026-02-03 16:06:59 +1100

    Add helper for meta key options

--- e2e/test/scenarios/data-studio/transforms/transforms.cy.spec.ts
+++ e2e/test/scenarios/data-studio/transforms/transforms.cy.spec.ts
@@ -2314,9 +2314,7 @@ LIMIT
         });
         cy.button("Create a transform").click();
         H.popover().findByText("Python script").click();
-        cy.get(".cm-clickable-token")
-          .should("be.visible")
-          .click({ metaKey: true });
+        cy.get(".cm-clickable-token").should("be.visible").click(H.holdMetaKey);
 
         cy.get("@windowOpen").should(
           "have.been.calledWithMatch",
